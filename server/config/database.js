398471380e515e69cce471d93c031a823da480f3
Agarvv <casluagarv@gmail.com>
2024-09-27 01:02:02 +0200

    trying to connect the db

--- server/config/database.js
+++ server/config/database.js
@@ -2,6 +2,12 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
+// Imprime las variables de entorno para depuración
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_HOST:', process.env.DB_HOST);
+
 // Ruta al certificado SSL
 const sslCA = fs.readFileSync(path.resolve(__dirname, 'certs/ca.pem'), 'utf8');
 
