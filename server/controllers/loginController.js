617d17c8afd17fd37fa9269e48daea0d2666158a
Agarvv <casluagarv@gmail.com>
2024-09-28 17:13:11 +0200

    bugs

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -84,7 +84,7 @@ const sendPasswordResetUrl = async (email) => {
         
     } catch (error) {
         console.error(error);
-        throw new Error('controller error', error);
+        throw error;
     }
 };
 
