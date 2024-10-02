d75f7f98cb0c3c29f8d7c7e65afaaf0222955c81
Agarvv <casluagarv@gmail.com>
2024-10-02 20:06:49 +0200

    issues

--- new/src/components/ChatBox.vue
+++ new/src/components/ChatBox.vue
@@ -4,7 +4,7 @@
       <div class="main-header">
         <div class="user-details">
           <div class="user-img">
-            <img style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover;" :src="userProfilePicture" class="profile-img">
+            <img style="width: 50px; height: 50px; border-radius: 50%; object-fit: cover;" :src="user.profilePicture ? apiUrl + '/' + user.profilePicture : '/images/default.jpeg" class="profile-img">
           </div>
           <div class="user-username">
             
@@ -164,11 +164,6 @@ export default {
     };
   },
   computed: {
-    userProfilePicture() {
-      return this.user.profilePicture
-        ? `${apiUrl}/${this.user.profilePicture}`
-        : '/images/default.jpeg';
-    },
     currentUser: state => state.user
   },
   methods: {
