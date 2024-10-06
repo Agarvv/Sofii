6e13ea66bfedf71cca08066972d9bbe127853010
Agarvv <casluagarv@gmail.com>
2024-10-06 14:55:42 +0200

    removing strict comparations

--- new/src/services/searchService.js
+++ new/src/services/searchService.js
@@ -17,25 +17,25 @@ export async function handleSearch(query, currentUser) {
     
     if (response.ok) {
         data.results.results.users.forEach(user => {
-            user.following = user.followers.some(follower => follower.Follower.follower_id === currentUser.user_id);
+            user.following = user.followers.some(follower => follower.Follower.follower_id == currentUser.user_id);
             
             user.isYourFriend = user.friends.some(friend => 
-                friend.friend_one_id === currentUser.user_id || 
-                friend.friend_two_id === currentUser.user_id
+                friend.friend_one_id == currentUser.user_id || 
+                friend.friend_two_id == currentUser.user_id
             );
         });
 
 
         data.results.results.posts.forEach(post => {
-            post.isLiked = post.postLikes.some(like => like.user_id === currentUser.user_id);
-            post.isSaved = post.saved_post.some(saved => saved.user_id === currentUser.user_id);
+            post.isLiked = post.postLikes.some(like => like.user_id == currentUser.user_id);
+            post.isSaved = post.saved_post.some(saved => saved.user_id == currentUser.user_id);
         });
 
 
 
         data.results.results.videos.forEach(video => {
-            video.isLiked = video.video_likes.some(like => like.user_id === currentUser.user_id);
-            video.isSaved = video.videos_saved.some(saved => saved.user_id === currentUser.user_id);
+            video.isLiked = video.video_likes.some(like => like.user_id == currentUser.user_id);
+            video.isSaved = video.videos_saved.some(saved => saved.user_id == currentUser.user_id);
         });
 
         return data;
