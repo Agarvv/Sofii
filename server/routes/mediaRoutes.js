924a378fd5b87d504de2df4973d530071ad860d0
Agarvv <casluagarv@gmail.com>
2024-10-07 18:44:36 +0200

    changes

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -9,41 +9,37 @@ const { uploadImage, uploadVideo } = require('../config/cloudinary');
 
 router.post('/upload_media', (req, res) => {
     try {
-        const form = formidable({ multiples: false })
-        
+        const form = formidable({ multiples: false });
+
         form.parse(req, async (err, fields, files) => {
-        if (err) {
-            return res.status(400).json({ error: 'while processing form' });
-        }
-     }
-     console.log('files', files)
-     
-     if (files.postPicture && files.postPicture[0].filepath) {
+            if (err) {
+                return res.status(400).json({ error: 'while processing form' });
+            }
+
+            console.log('files', files);
+
+            if (files.postPicture && files.postPicture[0].filepath) {
                 console.log('Subiendo imagen desde:', files.postPicture[0].filepath);
                 const uploadResult = await uploadImage(files.postPicture[0].filepath);
                 postPictureUrl = uploadResult.secure_url;
             } else {
                 console.log('No se ha enviado una imagen');
             }
-     
-     
-     
-     if(files.image && files.image[0].filepath) {
-         const uploadRes = await uploadImage(files.image[0].filepath)
-         return res.status(200).json({
-             filepath: uploadRes.secure_url
-         })
-     } else if(files.video && files.video[0].filepath) {
-         const uploadRes = await uploadVideo(files.video[0].filepath)
-         return res.status(200).json({
-             filepath: uploadRes.secure_url
-         })
-     } 
-         
-    
 
-        
-    } catch(e) {
+            if (files.image && files.image[0].filepath) {
+                const uploadRes = await uploadImage(files.image[0].filepath);
+                return res.status(200).json({
+                    filepath: uploadRes.secure_url
+                });
+            } else if (files.video && files.video[0].filepath) {
+                const uploadRes = await uploadVideo(files.video[0].filepath);
+                return res.status(200).json({
+                    filepath: uploadRes.secure_url
+                });
+            }
+        });
+
+    } catch (e) {
         console.log('Error:', e);
         return res.status(500).json({
             error: "internal_server_error"
