90dd727e3f9cb46dc0eb0f9bbe99975b7a4b732d
Agarvv <casluagarv@gmail.com>
2024-10-07 18:37:02 +0200

    changes

--- server/routes/mediaRoutes.js
+++ server/routes/mediaRoutes.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const cloudinary = require('../config/cloudinary')
 const { formidable } = require('formidable'); 
-const { uploadImage, uploadVideo, uploadAudio } = require('../config/cloudinary');
+const { uploadImage, uploadVideo } = require('../config/cloudinary');
 
 
 router.post('/upload_media', (req, res) => {
@@ -16,10 +16,31 @@ router.post('/upload_media', (req, res) => {
             return res.status(400).json({ error: 'while processing form' });
         }
      }
+     console.log('files', files)
      
-     if(files.)
-        
-        
+     if (files.postPicture && files.postPicture[0].filepath) {
+                console.log('Subiendo imagen desde:', files.postPicture[0].filepath);
+                const uploadResult = await uploadImage(files.postPicture[0].filepath);
+                postPictureUrl = uploadResult.secure_url;
+            } else {
+                console.log('No se ha enviado una imagen');
+            }
+     
+     
+     
+     if(files.image && files.image[0].filepath) {
+         const uploadRes = await uploadImage(files.image[0].filepath)
+         return res.status(200).json({
+             filepath: uploadRes.secure_url
+         })
+     } else if(files.video && files.video[0].filepath) {
+         const uploadRes = await uploadVideo(files.video[0].filepath)
+         return res.status(200).json({
+             filepath: uploadRes.secure_url
+         })
+     } 
+         
+    
 
         
     } catch(e) {
