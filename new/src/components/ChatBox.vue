f187ec6f78ce2e068d289b95e8223a5cfae32bb2
Agarvv <casluagarv@gmail.com>
2024-10-09 01:06:47 +0200

    ui issues

--- new/src/components/ChatBox.vue
+++ new/src/components/ChatBox.vue
@@ -273,7 +273,7 @@ export default {
       console.log('message class', message);
       console.log('user', this.user)
       if (!message.message_user || !this.user) return '';
-      return message.message_user.id === this.user.user_id ? 'friend' : 'user';
+      return message.message_user.id === this.user.user_id ? 'user' : 'friend';
     },
     getMessageUserProfilePicture(message) {
       return message.message_user?.profilePicture
