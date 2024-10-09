0af004227eb8fcc7da13673022b0389768cd1d7f
Agarvv <casluagarv@gmail.com>
2024-10-10 00:21:11 +0200

    final issue, twitter auth

--- server/config/twitterPassport.js
+++ server/config/twitterPassport.js
@@ -9,24 +9,23 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-
 passport.use(new TwitterStrategy({
-    consumerKey: process.env.TWITTER_API_KEY,
-    consumerSecret: process.env.TWITTER_SECRET,
-    callbackURL: 'https://sofii.onrender.com/auth/twitter/callback',
-    includeEmail: true
-  },
-  (token, tokenSecret, profile, done) => {
-    return (req, res, next) => {
-        req.session.oauthRequestToken = token;
-        req.session.oauthRequestTokenSecret = tokenSecret;
-        const user = {
-          id: profile.id,
-          username: profile.username,
-          email: profile.emails ? profile.emails[0].value : null,
-          profilePicture: profile.photos[0].value
-        };
-        done(null, user);
-    }
-  }
+  consumerKey: process.env.TWITTER_API_KEY,
+  consumerSecret: process.env.TWITTER_SECRET,
+  callbackURL: 'https://sofii.onrender.com/auth/twitter/callback',
+  includeEmail: true
+},
+(token, tokenSecret, profile, done) => {
+  const user = {
+    id: profile.id,
+    username: profile.username,
+    email: profile.emails ? profile.emails[0].value : null,
+    profilePicture: profile.photos[0].value
+  };
+  
+  // req.session.oauthRequestToken = token; <-- only if necesary
+  // req.session.oauthRequestTokenSecret = tokenSecret; <-- same as above
+  
+  done(null, user); // Passport necesita que llames a 'done' aquí cuando termines.
+}
 ));
