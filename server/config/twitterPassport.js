9ae96790ac761dcbff586ac38fab15090e8aee2b
Agarvv <casluagarv@gmail.com>
2024-10-10 00:03:15 +0200

    possible final commit... posible all is finished

--- server/config/twitterPassport.js
+++ server/config/twitterPassport.js
@@ -13,7 +13,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_API_KEY,
     consumerSecret: process.env.TWITTER_SECRET,
-    callbackURL: '',
+    callbackURL: 'https://sofii.onrender.com/auth/twitter/callback',
     includeEmail: true
   },
   (token, tokenSecret, profile, done) => {
