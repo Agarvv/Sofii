301f8911569b6c56256c5d1435ce93c305550f98
Agarvv <casluagarv@gmail.com>
2024-12-19 20:34:33 +0100

    database connection

---
+++ migrated-server/src/config/database.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize';
+
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host: process.env.DB_HOST!,
+    port: process.env.DB_PORT!,
+    dialect: 'mysql',
+    logging: true,
+    dialectOptions: {
+      connectTimeout: 60000
+    }
+  }
+);
+
+export default sequelize;
