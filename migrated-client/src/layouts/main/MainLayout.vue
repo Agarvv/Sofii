54af553bdea74b473c054b0506dee2eb845c6631
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-18 17:24:16 +0000

    testing if all is working

---
+++ migrated-client/src/layouts/main/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="layout-container">
+      <HeaderComponent></HeaderComponent>
+  
+      <main>
+        <router-view />
+      </main>
+  
+    </div>
+  </template>
+  
+  <script lang="ts">
+   import { defineComponent } from 'vue'
+   import HeaderComponent from '@/shared/header/HeaderComponent.vue';
+  
+   export default defineComponent({
+      name: 'MainLayout',
+      components: {
+          HeaderComponent, 
+      },
+   });
+  </script>
+  
+  <style src="./MainLayout.css"></style>
+  
---
+++ migrated-client/src/shared/header/HeaderComponent.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+      <header class="header">
+        <div class="first-flex">
+          <div class="f-flex-logo">
+            <p>Sofii</p>
+          </div>
+          <div class="f-flex-search">
+            <font-awesome-icon icon="search" />
+            <input type="search" placeholder="Search Users, Posts, Videos....." />
+          </div>
+        </div>
+  
+        <div class="responsive-aside-open">
+          <font-awesome-icon icon="bars" />
+        </div>
+  
+        <div class="m-flex-nav-items">
+          <div class="icon-div home">
+            <font-awesome-icon icon="home" />
+          </div>
+          <div class="icon-div create">
+            <font-awesome-icon icon="plus" />
+          </div>
+        </div>
+  
+        <div class="second-flex">
+          <div class="s-flex-user">
+            <div class="icon-div notifications">
+              <font-awesome-icon icon="bell" />
+              <div class="not-readed"></div>
+            </div>
+            <div class="icon-div chat">
+              <font-awesome-icon icon="comment" />
+            </div>
+            <div class="user-img">
+              <img
+                style="width: 45px; height: 45px; border-radius: 50%; object-fit: cover;"
+                src="soon"
+                alt="User Profile Picture"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+  
+      <div class="responsive-sub-header">
+        <div
+          class="rsb-i"
+          :class="{ on: activePage === 'home' }"
+        >
+          <font-awesome-icon icon="home" />
+        </div>
+        <div
+          class="rsb-i"
+          :class="{ on: activePage === 'watch' }"
+        >
+          <font-awesome-icon icon="tv" />
+        </div>
+        <div
+          class="rsb-i"
+          :class="{ on: activePage === 'create' }"
+        >
+          <font-awesome-icon icon="plus" />
+        </div>
+        <div
+          class="rsb-i"
+          :class="{ on: activePage === 'notifications' }"
+        >
+          <font-awesome-icon icon="bell" />
+        </div>
+        <div
+          class="rsb-i"
+          :class="{ on: activePage === 'chat' }"
+        >
+          <font-awesome-icon icon="comment-dots" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  
+  export default defineComponent({
+    name: 'HeaderComponent',
+    props: {
+      activePage: {
+        type: String as PropType<'home' | 'watch' | 'create' | 'notifications' | 'chat'>,
+        required: false,
+        value: 'home'
+      },
+    },
+  });
+  </script>
+  
+  <style src="./Header.css"></style>
+  
