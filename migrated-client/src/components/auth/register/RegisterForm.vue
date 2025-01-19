884fd7c39a8c6b9cd329ed8fab20b2cf5e9488fa
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-19 13:51:52 +0000

    issues

--- migrated-client/src/components/auth/register/RegisterForm.vue
+++ migrated-client/src/components/auth/register/RegisterForm.vue
@@ -1,67 +1,66 @@
 <template>
-    <div>
-      <div class="container">
-        <div class="wrapper">
-          <div class="login-form">
-            <h1 class="lf-h1">Welcome To Sofii!</h1>
-            <form @submit.prevent="handleSubmit(onSubmit)">
-              <div class="inp-box">
-                <input
-                  v-model="values.username"
-                  type="text"
-                  placeholder="Username"
-                />
-                <i class="fa fa-user icon"></i>
-                <span v-if="errors.username" class="val-error">{{ errors.username }}</span>
-              </div>
-              <div class="inp-box">
-                <input
-                  v-model="values.email"
-                  type="email"
-                  placeholder="Email"
-                />
-                <i class="fa fa-envelope icon"></i>
-                <span v-if="errors.email" class="val-error">{{ errors.email }}</span>
-              </div>
-              <div class="inp-box">
-                <input
-                  v-model="values.password"
-                  type="password"
-                  placeholder="Secure Password"
-                />
-                <i class="fa fa-lock icon"></i>
-                <span v-if="errors.password" class="val-error">{{ errors.password }}</span>
-              </div>
-              <div class="btn-box">
-                <button type="submit">
-                  Register
-                  <i class="fa fa-arrow-left"></i>
-                </button>
-              </div>
-              <div class="form-links">
-                <div>
-                  <a href="">Already Have An Account?</a>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="login-social-media">
-            <div class="social-buttons">
-              
+  <div>
+    <div class="container">
+      <div class="wrapper">
+        <div class="login-form">
+          <h1 class="lf-h1">Welcome To Sofii!</h1>
+          <form @submit.prevent="handleSubmit(onSubmit)">
+            <div class="inp-box">
+              <input
+                v-model="values.username"
+                type="text"
+                placeholder="Username"
+              />
+              <i class="fa fa-user icon"></i>
+              <span v-if="errors.username" class="val-error">{{ errors.username }}</span>
             </div>
+            <div class="inp-box">
+              <input
+                v-model="values.email"
+                type="email"
+                placeholder="Email"
+              />
+              <i class="fa fa-envelope icon"></i>
+              <span v-if="errors.email" class="val-error">{{ errors.email }}</span>
+            </div>
+            <div class="inp-box">
+              <input
+                v-model="values.password"
+                type="password"
+                placeholder="Secure Password"
+              />
+              <i class="fa fa-lock icon"></i>
+              <span v-if="errors.password" class="val-error">{{ errors.password }}</span>
+            </div>
+            <div class="btn-box">
+              <button type="submit">
+                Register
+                <i class="fa fa-arrow-left"></i>
+              </button>
+            </div>
+            <div class="form-links">
+              <div>
+                <a href="">Already Have An Account?</a>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="login-social-media">
+          <div class="social-buttons">
+            <!-- Social media buttons here -->
           </div>
         </div>
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
 import * as yup from 'yup';
-import { useForm } from 'vee-validate';
+import { useForm, useField } from 'vee-validate';
 import { apiService } from '@/api/ApiService';
 import { usePost } from '@/composables/usePost';
-
 
 interface RegisterFormValues {
   username: string;
@@ -78,18 +77,23 @@ export default defineComponent({
       password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
     });
 
-    const { handleSubmit, values, errors } = useForm<RegisterFormValues>({
+    const { handleSubmit, values, errors, validate } = useForm<RegisterFormValues>({
       validationSchema: schema,
     });
 
-    const onSubmit = (formData: RegisterFormValues) => {
-      console.log('Form Submitted:', formData);
-      const { data } = usePost<RegisterFormValues>({
+    const onSubmit = async (formData: RegisterFormValues) => {
+      const isValid = await validate();
+      if (isValid) {
+        console.log('Form Submitted:', formData);
+        const { data } = usePost<RegisterFormValues>({
           serviceFunc: (data: RegisterFormValues) => apiService.post('/auth/register', data),
           withError: true,
-          withLoading: true 
-      })
-      console.log('data', data)
+          withLoading: true,
+        });
+        console.log('data', data);
+      } else {
+        console.log('Form is not valid.');
+      }
     };
 
     return {
@@ -102,6 +106,4 @@ export default defineComponent({
 });
 </script>
 
-
 <style scoped src="./RegisterForm.css"></style>
-
