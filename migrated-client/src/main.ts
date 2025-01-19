1301b9ba3cde1b3f720232f9c59c1473bb1f5507
Agarvv <casluagarv@gmail.com>
2025-01-19 17:23:07 +0100

    issues

--- migrated-client/src/main.ts
+++ migrated-client/src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin, createQueryClient } from '@tanstack/vue-query'
 
+
+const queryClient = createQueryClient()
 
 createApp(App)
-.use(router)
-.use(createPinia())
-.mount('#app')
+  .use(router)                      
+  .use(createPinia())               
+  .use(VueQueryPlugin, { queryClient }) 
+  .mount('#app')
