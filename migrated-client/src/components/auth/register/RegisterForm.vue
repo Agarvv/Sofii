ae9a2ba1a4a88834e06455e26a4a1b8e22d9d11c
Agarvv <casluagarv@gmail.com>
2025-01-19 17:59:25 +0100

    issues

--- migrated-client/src/components/auth/register/RegisterForm.vue
+++ migrated-client/src/components/auth/register/RegisterForm.vue
@@ -59,8 +59,8 @@
 import { defineComponent } from 'vue';
 import * as yup from 'yup';
 import { useForm } from 'vee-validate';
+import { useMutation } from '@tanstack/vue-query';
 import { apiService } from '@/api/ApiService';
-import { usePost } from '@/composables/usePost';
 
 interface RegisterFormValues {
   username: string;
@@ -78,23 +78,28 @@ export default defineComponent({
       password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
     });
 
-
     const { handleSubmit, values, errors, validate } = useForm<RegisterFormValues>({
       validationSchema: schema,
     });
 
-  
-    const { mutate } = usePost<RegisterFormValues>({
-      serviceFunc: (data: RegisterFormValues) => apiService.post('/auth/register', data),
-      withError: true,
-      withLoading: true,
+    const mutation = useMutation({
+      mutationFn: (data: RegisterFormValues) => apiService.post('/auth/register', data),
+      onMutate: () => {
+        console.log("Mutating...");
+      },
+      onError: (error: any) => {
+        console.error("Error: ", error);
+      },
+      onSuccess: (response: any) => {
+        console.log("Success:", response);
+      }
     });
 
     const onSubmit = async () => {
       const isValid = await validate(); 
       if (isValid) {
         console.log('Form Submitted:', values); 
-        mutate(values);
+        mutation.mutate(values);
       } else {
         console.log('Form is not valid.');
       }
@@ -105,10 +110,10 @@ export default defineComponent({
       values,
       errors,
       onSubmit,
+      mutation, 
     };
   },
 });
 </script>
-
 
 <style scoped src="./RegisterForm.css"></style>
--- migrated-client/src/main.ts
+++ migrated-client/src/main.ts
@@ -1,9 +1,9 @@
-import { VueQueryPlugin } from '@tanstack/vue-query';
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
 
-// const queryClient = new QueryClient(); 
+const queryClient = new QueryClient(); 
 
 createApp(App)
   .use(router)
   .use(createPinia())
-  .use(VueQueryPlugin)
+  .use(VueQueryPlugin, { queryClient })
   .mount('#app');
