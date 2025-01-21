e02a3da093684b8cb4b5cb65bf974a89ef58cd9a
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-21 20:31:35 +0000

    issues

--- migrated-client/src/composables/useGet.ts
+++ migrated-client/src/composables/useGet.ts
@@ -16,7 +16,7 @@ export function useGet<T>({ serviceFunc, successFunc, withError }: UseGetOptions
     queryFn: serviceFunc, 
   })
 
-  console.log("datas", data?.value)  
+  console.log("datas", data)  
 
   if (data?.value) {  
     successFunc ? successFunc(data.value) : console.log('Get Succeeded!', data.value)
