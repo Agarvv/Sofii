1067f5fe372e82f3b1e6060157bb4f62d5098c54
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-21 21:26:38 +0000

    issues

--- migrated-client/src/views/home/HomeView.vue
+++ migrated-client/src/views/home/HomeView.vue
@@ -7,11 +7,11 @@
       
       <main class="main">
         <div class="posts" v-if="data?.posts?.length">
-          <PostCard 
+         <!--  <PostCard 
             v-for="post in data.posts" 
             :key="post.id" 
             :post="post"
-          />
+          /> -->
         </div>
         <div v-else>
           <p>No posts.</p>
@@ -19,7 +19,7 @@
       </main>
       
       <div class="right-aside">
-        <UsersMayLike :recomendedUsers="data?.users || []" /> 
+        <!-- <UsersMayLike :recomendedUsers="data?.users || []" />  -->
       </div>
     </div>
   </template>
@@ -28,19 +28,19 @@
   import { defineComponent } from 'vue'; 
   import HeaderComponent from '@/shared/header/HeaderComponent.vue';
   import AsideComponent from '@/shared/aside/AsideComponent.vue';
-  import UsersMayLike from '@/components/home/users-may-like/UsersMayLike.vue';
+  //import UsersMayLike from '@/components/home/users-may-like/UsersMayLike.vue';
   import { useGet } from '@/composables/useGet';
   import { Post } from '@/types/posts/Post';
   import { UserMayLike } from '@/types/users/UserMayLike';
-  import PostCard from '@/shared/post/PostCard.vue';
+  //import PostCard from '@/shared/post/PostCard.vue';
   
   export default defineComponent({
     name: 'HomeView',
     components: {
       HeaderComponent,
       AsideComponent,
-      UsersMayLike,
-      PostCard 
+     // UsersMayLike,
+     // PostCard 
     },
     async setup() {
       const data = await useGet<{ posts: Post[], users: UserMayLike[] }>({
