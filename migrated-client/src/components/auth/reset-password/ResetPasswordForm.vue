a13c429569955b6f3119b1f004834846266fae43
Agarvv <casluagarv@gmail.com>
2025-01-21 02:34:49 +0100

    issues

--- migrated-client/src/components/auth/reset-password/ResetPasswordForm.vue
+++ migrated-client/src/components/auth/reset-password/ResetPasswordForm.vue
@@ -1,91 +1,56 @@
 <template>
-    <div>
-      <div class="out">
-        <div class="container">
-          <h2>Change Your Password</h2>
-          
-          <form @submit.prevent="handleSubmit">
-            <div class="inp-box">
-              <input v-model="password" type="password" placeholder="New Password" required />
-              <i class="fas fa-lock icon"></i>
-              <span v-if="errors.password" class="val-error">{{ errors.password }}</span>
-            </div>
-  
-            <div class="btn-box">
-              <button type="submit">
-                Update Your Password
-                <i class="fas fa-arrow-left"></i>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+<div>
+  <div class="out">
+      <div class="container">
+        <h2>Change Your Password</h2>
+         <form @submit.prevent="handleSubmit"> 
+            <input
+            v-model="password" 
+            type="password" 
+            placeholder="New Password"
+            required>
+            
+            <button type="submit">Update Your Password</button>
+          </form> 
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { useForm, useField } from 'vee-validate';
-import * as yup from 'yup';
-import { apiService } from '@/api/ApiService';
-import { usePost } from '@/composables/usePost';
+  </div>
+</div>
+</template>
+
+
+<script lang="ts">
+import { ref } from "vue";
+import { useForm, useField } from "vee-validate";
+import * as yup from "yup";
 
 interface ResetPasswordFormValues {
-  email: string;
-  token: string;
-  password: string; 
+  password: string;
 }
 
-export default defineComponent({
-  name: 'ResetPassword',
+export default {
+  name: 'ResetPasswordForm', 
   setup() {
-    const route = useRoute();
-    const email = ref<string>('');  
-    const token = ref<string>('');  
-    const { value: password, errorMessage } = useField('password', yup.string().min(6, "Password must have at least 6 chars").required("Password is required"));
-
-    const loading = ref(false);
-
-    onMounted(() => {
-      email.value = route.params.email as string;
-      token.value = route.params.token as string;
+    const validationSchema = yup.object({
+      password: yup.string().min(6, "Password must have at least 6 characters").required("Password is required"),
     });
 
-    const { handleSubmit } = useForm<ResetPasswordFormValues>({
-      initialValues: {
-        password: '',
-        email: '',
-        token: ''
-      }
+    const { handleSubmit, errors } = useForm<ResetPasswordFormValues>({
+      validationSchema,
     });
 
-    const { mutate } = usePost<ResetPasswordFormValues>({
-      serviceFunc: (data: ResetPasswordFormValues) => apiService.post('/auth/reset-password', data),
-      successMessage: 'Your Password Is Set!',
-      withError: true,
-      withLoading: true,
-    });
+    const { value: password } = useField("password");
 
-    const onSubmit = async (values: ResetPasswordFormValues) => {
-      console.log("Form submitted:", values);
-      await mutate(values);
+    const onSubmit = (values: ResetPasswordFormValues) => {
+      console.log("Password reset submitted:", values);
     };
 
     return {
-      email,
-      token,
       password,
-      errorMessage,
+      errors,
       handleSubmit: handleSubmit(onSubmit),
-      loading,
     };
   },
-});
+};
 </script>
 
-
-
-<style scoped src="./ResetPasswordForm.css">
-
-</style>
+<style src="./ResetPasswordForm.css"></style>
