8bcb1956a92295d4292c5d55b89e1073bc0a945a
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-21 19:56:06 +0000

    issues

--- migrated-client/src/composables/useGet.ts
+++ migrated-client/src/composables/useGet.ts
@@ -10,6 +10,7 @@ interface UseGetOptions<T> {
 export function useGet<T>({ serviceFunc, successFunc, withError }: UseGetOptions<T>) {
   const apiStore = apiStatusStore() 
   console.log("use get called")
+  
   const { data, error } = useQuery<T>({
     queryKey: ['data'],   
     queryFn: serviceFunc, 
@@ -19,8 +20,8 @@ export function useGet<T>({ serviceFunc, successFunc, withError }: UseGetOptions
     successFunc ? successFunc(data.value) : console.log('Get Succeeded!', data.value)
   }
 
-  if (error && withError) {
-    console.log("ERROR", error)
+  if (error.value && withError) {
+    console.log("ERROR", error.value) 
     apiStore.setError('Something Went Wrong... :c') 
   }
 
