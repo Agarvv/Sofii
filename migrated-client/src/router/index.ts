3f1cb9d191d1e9ee0e1c97ddbc3869d5e25a9318
Agarvv <casluagarv@gmail.com>
2025-01-21 01:22:22 +0100

    protected routes

--- migrated-client/src/router/index.ts
+++ migrated-client/src/router/index.ts
@@ -5,7 +5,7 @@ import RegisterView from '@/views/auth/RegisterView.vue'
 import LoginView from '@/views/auth/LoginView.vue'
 import SendResetPasswordView from '@/views/auth/SendResetPasswordView.vue'
 import ResetPasswordView from '@/views/auth/ResetPasswordView.vue'
-
+import axios from 'axios'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -35,16 +35,42 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: '/',
         component: HomeView,
-        name: "home"
+        name: "home",
+        meta: { requiresAuth: true }
       }
     ]
   }
-
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+
+const isAuthenticated = async (): Promise<boolean> => {
+  try {
+    const response = await axios.get('/api/auth/verify'); 
+    
+    return response.status == 200; 
+    
+  } catch (error) {
+    return false;  
+  }
+}
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const authenticated = await isAuthenticated();  
+
+    if (!authenticated) {
+      next({ name: 'login' });
+    } else {
+      next(); 
+    }
+  } else {
+    next();  
+  }
+});
 
 export default router
