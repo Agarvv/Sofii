8edd472ea8edf8b82047c87838a7429a8c89512d
Agarvv <casluagarv@gmail.com>
2025-01-22 18:35:39 +0100

    issues

---
+++ migrated-client/src/components/create/CreateForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="create-container">
+            <div class="f-column">
+                <div class="fc-inp"> 
+                    <input v-model="content" placeholder="What's Up?">
+                </div>
+            </div>
+            <div class="demostration-content">
+                <img 
+                style="width: 100%; height: 250px; object-fit: cover; border-radius: 15px;" 
+                src="imageSource">
+            </div>
+            <div class="s-column">
+                <button type="submit"> 
+                  <div class="sce-three">
+                     <i class="fa fa-paper-plane"></i>
+                  </div>
+                </button> 
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    
+    export default defineComponent({
+        name: 'CreateForm'
+    })
+</script>
+
+<style scoped src="./CreateForm.css"></style>
---
+++ migrated-client/src/components/create/post-image/PostImage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div @click="openFileInput" class="sce-one">
+        <i class="fa fa-image"></i>
+        <input style="display: none" @change="handleImageChange" type="file" id="imageInp">
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    
+    export default defineComponent({
+        name: 'PostImage'
+    })
+</script>
+
+<style scoped src="./PostImage.css" >
+    
+</style>
