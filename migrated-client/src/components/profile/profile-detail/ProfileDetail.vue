c1f26e20f30afee3d6454cb5a60acd94ce35ea96
Agarvv <casluagarv@gmail.com>
2025-01-23 23:37:52 +0100

    profile page

---
+++ migrated-client/src/components/profile/profile-detail/ProfileDetail.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="detail">
+        <i v-if="icon" :class="icon"></i>
+        <span>{{ detail }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+    
+    export default defineComponent({
+        name: 'ProfileDetail',
+        props: {
+            detail: {
+                type: String,
+                required: true 
+            },
+            icon: {
+                type: String,
+                required: false 
+            }
+        }
+    })
+</script>
--- migrated-client/src/views/profile/ProfileView.vue
+++ migrated-client/src/views/profile/ProfileView.vue
@@ -1,138 +1,133 @@
 <template>
-  <div>
-    <p>profile soon</p>
-  </div>
-   <!--  <div>
-      <ProfileHeader />
-      
-      <div class="responsive-user-details">
-          
-          <div class="r-bio">
-              <p>{{user.bio ? user.bio : 'BIO Not Provided'}}</p>
-          </div>
-          
-          <div class="r-info">
-              <div class="ubication">
-                <font-awesome-icon icon="fas fa-map-marker-alt" style="color: red" />
-                <span>{{ user.ubication ? user.ubication : 'Ubication Not Provided' }}</span>
-              </div>
-              
-              <div class="native">
-                <font-awesome-icon icon="fas fa-globe" style="color: black" />
-                <span>{{ user.native_city ? user.native_city : 'Native City Not Provided' }}</span>
-              </div>
-              
-              <div class="gender">
-                <font-awesome-icon icon="fas fa-venus-mars" style="color: hotpink" />
-                <span>{{ user.gender ? user.gender : 'Gender Not Provided' }}</span>
-              </div>
-              
-              <div class="work">
-                <font-awesome-icon icon="fas fa-briefcase" style="color: gray" />
-                <span>{{ user.job ? user.job : 'Job Not Provided' }}</span>
-              </div>
-              
-              <div class="love">
-                <font-awesome-icon icon="fa fa-heart" style="color: red" />
-                <span>{{ user.civil_status ? user.civil_status : 'Civil Status Not Provided' }}</span>
-              </div>
-              
-          </div>
-          
-      </div>
-  
-      <div class="container">
-        <aside>
-          <div class="user-info">
-            <div class="info">
-              <div class="info-header">
-                <h4>Info</h4>
-              </div>
-             
-             <div class="user-i">
-                 
-                 
-               <div  class="description">
-                 <p>{{ user.bio ? user.bio  : 'Bio Not Provided' }}</p>
-               </div>
-               
-              <div class="ubication">
-                <font-awesome-icon icon="fas fa-map-marker-alt" style="color: red" />
-                <span>{{ user.ubication ? user.ubication : 'Ubication Not Provided' }}</span>
-              </div>
-              
-              <div class="native">
-                <font-awesome-icon icon="fas fa-globe" style="color: black" />
-                <span>{{ user.native_city ? user.native_city : 'Native City Not Provided' }}</span>
-              </div>
-              
-              <div class="gender">
-                <font-awesome-icon icon="fas fa-venus-mars" style="color: hotpink" />
-                <span>{{ user.gender ? user.gender : 'Gender Not Provided' }}</span>
-              </div>
-              
-              <div class="work">
-                <font-awesome-icon icon="fas fa-briefcase" style="color: gray" />
-                <span>{{ user.job ? user.job : 'Job Not Provided' }}</span>
-              </div>
-              
-              <div class="love">
-                <font-awesome-icon icon="fa fa-heart" style="color: red" />
-                <span>{{ user.civil_status ? user.civil_status: 'Civil Status Not Provided' }}</span>
-              </div>
-              
-              
-             </div>
-            </div>
-          </div>
-        </aside>
-  
-        <main>
-            <h4 style="width: 100%;
-             display: flex;
-             align-items: center;
-             justify-content: center;
-             font-size: 20px;
-             padding: 10px;
-             color: gray" v-if="userPosts.length == 0">This User Does Not Have Posts...</h4>
-               <div v-if="user.posts.length > 0" class="posts">
-                  
-            <div  v-for="post in user.posts" :key="post.id" class="post" >
-                <PostCard :post="post"
-                  @delete="handlePostRemoval"
-                />
-            </div>
+  <div v-if="data">
+    <ProfileHeader :profile="data.profile" />
 
-          </div> 
-        </main>
+    <div class="responsive-user-details">
+      <ProfileDetail 
+        :detail="data.profile.bio || 'Not Provided'" 
+      />
+      
+      <div class="r-info">
+        <ProfileDetail 
+          :detail="data.profile.ubication || 'Ubication Not Provided'" 
+          icon="fa fa-map-marker-alt" 
+          iconColor="red" 
+        />
+        <ProfileDetail 
+          :detail="data.profile.native_city || 'Native City Not Provided'" 
+          icon="fa fa-globe" 
+          iconColor="black" 
+        />
+        <ProfileDetail 
+          :detail="user.gender || 'Gender Not Provided'" 
+          icon="fas fa-venus-mars" 
+          iconColor="hotpink" 
+        />
+        <ProfileDetail 
+          :detail="user.job || 'Job Not Provided'" 
+          icon="fas fa-briefcase" 
+          iconColor="gray" 
+        />
+        <ProfileDetail 
+          :detail="user.civil_status || 'Civil Status Not Provided'" 
+          icon="fa fa-heart" 
+          iconColor="red" 
+        />
       </div>
-    </div> -->
-  </template>
+    </div>
+
+    <div class="container">
+      <aside>
+        <div class="user-info">
+          <div class="info">
+            <div class="info-header">
+              <h4>Info</h4>
+            </div>
+            <div class="user-i">
+              <ProfileDetail 
+                :detail="user.bio || 'Bio Not Provided'" 
+              />
+              <ProfileDetail 
+                :detail="user.ubication || 'Ubication Not Provided'" 
+                icon="fas fa-map-marker-alt" 
+                iconColor="red" 
+              />
+              <ProfileDetail 
+                :detail="user.native_city || 'Native City Not Provided'" 
+                icon="fas fa-globe" 
+                iconColor="black" 
+              />
+              <ProfileDetail 
+                :detail="user.gender || 'Gender Not Provided'" 
+                icon="fas fa-venus-mars" 
+                iconColor="hotpink" 
+              />
+              <ProfileDetail 
+                :detail="user.job || 'Job Not Provided'" 
+                icon="fas fa-briefcase" 
+                iconColor="gray" 
+              />
+              <ProfileDetail 
+                :detail="user.civil_status || 'Civil Status Not Provided'" 
+                icon="fa fa-heart" 
+                iconColor="red" 
+              />
+            </div>
+          </div>
+        </div>
+      </aside>
+
+      <main>
+        <div v-if="user.posts.length > 0" class="posts">
+          <div v-for="post in data.profile.posts" :key="post.id" class="post">
+              
+            <PostCard 
+              :post="post" 
+            />
+            
+          </div>
+        </div>
+      </main>
+      
+      
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
-  import { defineComponent, onMounted } from 'vue';
+  import { defineComponent, onMounted, ref } from 'vue';
   import ProfileHeader from '@/components/profile/profile-header/ProfileHeader.vue';
   import { useGet } from '@/composables/useGet';
   import { useRoute } from 'vue-router';
   import { Profile } from '@/types/profile/Profile';
+  import ProfileDetail from '@/components/profile/profile-detail/ProfileDetail.vue'
+  import PostCard from '@/shared/post/PostCard.vue'
 
   export default defineComponent({
     name: 'ProfileView',
+    components: {
+        ProfileDetail,
+        PostCard 
+    }, 
     setup() {
       const route = useRoute(); 
-
+      const data = ref<Profile | null>(null);
+      
+      
       const getProfile = async () => {
         const id = route.params.id as string; 
         if(id == "s") {
           console.log("SELF");
         }
 
-        const data = await useGet<Profile>({
+        const fetchedData = await useGet<Profile>({
            endpoint: `/profile/${id}`,
            withError: true 
         });
 
-        console.log('profile data', data);
+        data.value = fetchedData; 
+
+        console.log('profile data', fetchedData);
       };
 
       onMounted(() => {
@@ -140,8 +135,11 @@
       });
 
       return {
+        data,
         getProfile,
       };
     },
   });
 </script>
+
+<style src="./ProfileView.css"></style>
