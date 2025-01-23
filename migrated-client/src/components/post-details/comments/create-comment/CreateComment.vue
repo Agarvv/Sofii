26572d8a6e043438368c7676e5b73ea7d0ec98dc
Agarvv <casluagarv@gmail.com>
2025-01-23 14:59:46 +0100

    issues

--- migrated-client/src/components/post-details/comments/create-comment/CreateComment.vue
+++ migrated-client/src/components/post-details/comments/create-comment/CreateComment.vue
@@ -1,73 +1,70 @@
 <template>
-   <div class="upload-comment">
-      <form @submit.prevent="handleSubmit(onSubmit)"> 
-            <div class="input">
-              <input v-model="commentValue" type="text" placeholder="Upload A Comment">
-            </div>
-            
-            <button type="submit" class="send-button">
-               <div class="send-button">
-                  <i class="fa fa-paper-plane"></i>
-               </div>
-            </button>
-      </form>
-   </div>
+  <div class="upload-comment">
+    <form @submit.prevent="handleSubmit">
+      <div class="input">
+        <input v-model="commentValue" type="text" placeholder="Upload A Comment" />
+      </div>
+      <button type="submit" class="send-button">
+        <div class="send-button">
+          <i class="fa fa-paper-plane"></i>
+        </div>
+      </button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, toRefs } from 'vue'; 
-    import { useForm, useField } from 'vee-validate';
-    import * as yup from 'yup';
-    import { usePost } from '@/composables/usePost';
-    import { apiService } from '@/api/ApiService';
+import { defineComponent, toRefs } from 'vue';
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
+import { usePost } from '@/composables/usePost';
+import { apiService } from '@/api/ApiService';
 
-    export default defineComponent({
-        name: 'CreateComment',
-        props: {
-            postId:  {
-                type: Number,
-                required: true 
-            }
-        },
-        setup(props) {
-            interface CommentValues {
-                postId: number,
-                commentValue: string 
-            }
-            
-            const validationSchema = yup.object({
-                postId: yup.number().required(),
-                commentValue: yup.string().required("Comment is required")
-            })
-            
-            const { handleSubmit, errors } = useForm<CommentValues>({
-                  validationSchema,
-            });
-            
-            const { value: commentValue } = useField('commentValue');
-            const { postId } = toRefs(props);
+export default defineComponent({
+  name: 'CreateComment',
+  props: {
+    postId: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    interface CommentValues {
+      postId: number;
+      commentValue: string;
+    }
 
-            const { mutate } = usePost<CommentValues>({
-               serviceFunc: (data: CommentValues) => apiService.post('/posts/comments', data),
-               successFunc: () => window.location.reload(),
-               withError: true,
-               withLoading: true,
-            });
-            
-            const onSubmit = async (values: CommentValues) => {
-                console.log("Comment submitted", values);
-                await mutate(values); 
-            }
-            
-            return { 
-                commentValue,
-                handleSubmit,
-                onSubmit,
-                errors,
-            };
-        }
+    const validationSchema = yup.object({
+      postId: yup.number().required(),
+      commentValue: yup.string().required('Comment is required'),
     });
-</script>
 
+    const { handleSubmit, errors } = useForm<CommentValues>({
+      validationSchema,
+    });
+
+    const { value: commentValue } = useField('commentValue');
+    const { postId } = toRefs(props);
+
+    const { mutate } = usePost<CommentValues>({
+      serviceFunc: (data: CommentValues) => apiService.post('/posts/comments', data),
+      successFunc: () => window.location.reload(),
+      withError: true,
+      withLoading: true,
+    });
+
+    const onSubmit = async (values: CommentValues) => {
+      console.log('Comment submitted', values);
+      await mutate(values);
+    };
+
+    return {
+      commentValue,
+      errors,
+      handleSubmit: handleSubmit(onSubmit), 
+    };
+  },
+});
+</script>
 
 <style scoped src="./CreateComment.css"></style>
