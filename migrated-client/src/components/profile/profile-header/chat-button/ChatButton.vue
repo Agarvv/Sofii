917a18018168614a9a8bbd5af1bd8b686d7820db
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-24 15:24:13 +0000

    issues

--- migrated-client/src/components/profile/profile-header/chat-button/ChatButton.vue
+++ migrated-client/src/components/profile/profile-header/chat-button/ChatButton.vue
@@ -1,40 +1,41 @@
 <template>
-     <button @click="startChat" style="background: purple;">
-           <i class="fa fa-comment"></i> Chat
-     </button>
-</template>
-
-<script lang="ts">
-    import { defineComponent } from 'vue'; 
-    import { usePost } from '@/composables/usePost';
-    import { apiService } from '@/api/ApiService';
-    
-    export default defineComponent({
-        name: 'ChatButton',
-        props: {
-            receiverId: {
-                type: Number,
-                required: true 
-            }
-        }, 
-        setup(props) {
-            interface ChatCreationValues {
-                receiverId: number 
-            }
-
-            const startChat = async () => {
-                const { mutate, data } = usePost<ChatCreationValues>({
-                  serviceFunc: (data: ChatCreationValues) => apiService.post('/chats', data),
-                  successFunc: () => console.log("Chat created!"),
-                  withError: true,
-                  withLoading: true 
-                })
-                
-                await mutate({ receiverId: props.receiverId })
-                console.log('data from chat', data)
-            }
-
-            return { startChat }
-        }
-    })
-</script>
+    <button @click="startChat" style="background: purple;">
+      <i class="fa fa-comment"></i> Chat
+    </button>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue'; 
+  import { usePost } from '@/composables/usePost';
+  import { apiService } from '@/api/ApiService';
+  
+  export default defineComponent({
+    name: 'ChatButton',
+    props: {
+      receiverId: {
+        type: Number,
+        required: true 
+      }
+    }, 
+    setup(props) {
+      interface ChatCreationValues {
+        receiverId: number
+      }
+  
+      const { mutate, data } = usePost<ChatCreationValues>({
+        serviceFunc: (data: ChatCreationValues) => apiService.post('/chats', data),
+        successFunc: () => console.log("Chat created!"),
+        withError: true,
+        withLoading: true
+      })
+  
+      const startChat = async () => {
+        await mutate({ receiverId: props.receiverId })
+        console.log('data from chat', data.value)  
+      }
+  
+      return { startChat }
+    }
+  })
+  </script>
+  
