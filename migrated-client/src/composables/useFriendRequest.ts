a5be265938941e04c268d803b07143a7e1dfbd0e
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-25 22:57:04 +0000

    issues

--- migrated-client/src/composables/useFriendRequest.ts
+++ migrated-client/src/composables/useFriendRequest.ts
@@ -3,22 +3,23 @@ import { apiService } from "@/api/ApiService";
 
 export default function useFriendRequest() {
   interface Options {
-    type: string, 
-    requestId: number
+    type: string; 
+    requestId: number;
   }
-
-  const { mutate } = usePost({
-    serviceFunc: (data: { requestId: number }) => apiService.post('/users/friendRequest/deny', data),
-    successFunc: () => window.location.reload(),
-    withError: true,
-    withLoading: true,
-  });
 
   const acceptOrDeny = async ({ type, requestId }: Options) => {
     const endpoint = type === "accept" ? "/users/friendRequest/accept" : "/users/friendRequest/deny";
+
+    const { mutate } = usePost({
+      serviceFunc: (data: { requestId: number }) => apiService.post(endpoint, data),
+      successFunc: () => window.location.reload(),
+      withError: true,
+      withLoading: true,
+    });
 
     await mutate({ requestId });
   };
 
   return { acceptOrDeny };
 }
+
