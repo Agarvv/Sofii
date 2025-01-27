2346b8b15b37a5d4552273e51eadadbb8d8a62ea
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-01-27 23:43:29 +0000

    messages owners

--- migrated-client/src/components/chat/chatbox/message/ChatMessage.vue
+++ migrated-client/src/components/chat/chatbox/message/ChatMessage.vue
@@ -1,38 +1,44 @@
 <template>
-       <div 
-                class="message user"
-            >
-                <div class="msg_content">
-                    <div 
-                        v-if="message.message_content.length > 0" 
-                        class="message-user-img"
-                    >
-                        <img 
-                            src="getMessageUserProfilePicture(message)" 
-                            class="message-img"
-                        />
-                    </div>
-                    <p v-if="message.message_content.length > 0">
-                        {{ message.message_content }}
-                    </p>
-                    <i class="fa fa-check"></i>
-                </div>
-            </div>
-</template>
-
-<script lang="ts">
-    import { defineComponent } from "vue"
-    import { Message } from '@/types/chats/Message'
-    
+    <div :class="getMessageClass()">
+      <div class="msg_content">
+        <div v-if="message.message_content.length > 0" class="message-user-img">
+          <img :src="message.message_user.profilePicture" class="message-img" />
+        </div>
+        <p v-if="message.message_content.length > 0">
+          {{ message.message_content }}
+        </p>
+        <i class="fa fa-check" :style="{ color: message.readed ? 'blue' : '' }"></i>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+    import { defineComponent } from "vue";
+    import { Message } from "@/types/chats/Message";
+  
     export default defineComponent({
-        name: 'ChatMessage',
-        props: {
-            message: {
-                type: Object as () => Message,
-                required: true 
-            }
-        }
-    })
-</script>
-
-<style src="./ChatMessage.css"></style>
+      name: "ChatMessage",
+      props: {
+        message: {
+          type: Object as () => Message,
+          required: true,
+        },
+      },
+      setup(props) {
+        const getMessageClass = () => {
+          const userId = Number(localStorage.getItem("userId"));
+          if (!userId) return ""; 
+  
+          const isOwn = props.message.message_user.id == userId;
+          return isOwn ? "message user" : "message friend";
+        };
+  
+        return {
+          getMessageClass,
+        };
+      },
+    });
+  </script>
+  
+  <style src="./ChatMessage.css"></style>
+  
