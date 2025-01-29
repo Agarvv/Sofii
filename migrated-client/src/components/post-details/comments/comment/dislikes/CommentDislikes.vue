f5631eeb2e6e00ffeb4c53a89114a34c6e7e2ebd
Agarvv <casluagarv@gmail.com>
2025-01-29 02:20:16 +0100

    comment likes and dislikesX

--- migrated-client/src/components/post-details/comments/comment/dislikes/CommentDislikes.vue
+++ migrated-client/src/components/post-details/comments/comment/dislikes/CommentDislikes.vue
@@ -1,43 +1,80 @@
 <template>
-       <div v-if="dislikes" @click="dislike">
-           <i class="fa fa-thumbs-down"></i>
-           <p>{{ dislikes.length }}</p>
-       </div>
+    <div @click="dislike" class="dislike">
+        <i class="fa fa-thumbs-down" :style="{ color: isDisliked ? 'blue' : '' }"></i>
+        <p>{{ dislikesCount }}</p>
+    </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, PropType } from 'vue'
-    import { CommentDislikes } from '@/types/posts/CommentDislikes';
-    import { useDislike } from '@/composables/useDislike'
-    
-    export default defineComponent({
-        name: 'CommentDislikes',
-        props: {
-            dislikes: { 
-                type: Object as () => CommentDislikes[],
-                required: true 
-            },
-            postId: {
-                type: Number,
-                required: true 
-            },
-            commentId: {
-                type: Number,
-                required: true 
-            }
+import { defineComponent, ref, computed, onMounted, onBeforeUnmount } from 'vue';
+import { CommentDislikes } from '@/types/posts/CommentDislikes';
+import { useDislike } from '@/composables/useDislike';
+import { useSocket } from '@/composables/useWebSocket';
+
+export default defineComponent({
+    name: 'CommentDislikes',
+    props: {
+        dislikes: {
+            type: Array as () => CommentDislikes[],
+            required: true
         },
-        setup(props) {
-            const dislike = async () => {
-                const data = await useDislike({
-                    type: 'COMMENT',
-                    postId: props.postId,
-                    commentId: props.commentId 
-                })
-                console.log("Data from useDislike", data); 
-                
-            }
-            
-            return { dislike } 
+        postId: {
+            type: Number,
+            required: true
+        },
+        commentId: {
+            type: Number,
+            required: true
         }
-    })
+    },
+    setup(props) {
+        const userId = Number(localStorage.getItem('userId'));
+
+        const dislikes = ref<CommentDislikes[]>([...(props.dislikes || [])]);
+        
+        const isDisliked = ref<boolean>(dislikes.value.some(dislike => dislike.user_id === userId));
+        
+        const dislikesCount = computed(() => dislikes.value.length);
+
+        const { socket } = useSocket();
+
+        const dislike = async () => {
+            const data = await useDislike({
+                type: 'COMMENT',
+                postId: props.postId,
+                commentId: props.commentId
+            });
+            console.log("Data from useDislike", data);
+        };
+
+        onMounted(() => {
+            socket.instance.on("dislikeComment", (disliked: CommentDislikes) => {
+                if (disliked.comment_id === props.commentId) {
+                    dislikes.value.push(disliked);
+                    
+                    if (disliked.user_id === userId) isDisliked.value = true;
+                    
+                    console.log("Comment disliked via WebSocket:", disliked);
+                }
+            });
+
+            socket.instance.on("undislikeComment", (disliked: CommentDislikes) => {
+                if (disliked.comment_id === props.commentId) {
+                    dislikes.value = dislikes.value.filter(d => d.user_id !== disliked.user_id);
+                    
+                    if (disliked.user_id === userId) isDisliked.value = false;
+                    
+                    console.log("Comment undisliked via WebSocket:", disliked);
+                }
+            });
+        });
+
+        onBeforeUnmount(() => {
+            socket.instance.off("dislikeComment");
+            socket.instance.off("undislikeComment");
+        });
+
+        return { dislike, dislikesCount, isDisliked };
+    }
+});
 </script>
--- migrated-client/src/components/post-details/comments/comment/likes/CommentLikes.vue
+++ migrated-client/src/components/post-details/comments/comment/likes/CommentLikes.vue
@@ -1,43 +1,82 @@
 <template>
-      <div v-if="likes" @click="like" class="like">
-          <i class="fa fa-thumbs-up"></i>
-          <p>{{ likes.length }}</p>
-      </div>
+    <div @click="like" class="like">
+        <i class="fa fa-thumbs-up" :style="{ color: isLiked ? 'blue' : '' }"></i>
+        <p>{{ likesCount }}</p>
+    </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, PropType } from 'vue'
-    import { useLike } from '@/composables/useLike'
-    import { CommentLikes } from '@/types/posts/CommentLikes';
-    
-    
-    export default defineComponent({
-        name: 'CommentLikes',
-        props: {
-            likes: {
-                type: Object as () => CommentLikes[],
-                required: true 
-            },
-            postId: {
-                type: Number,
-                required: true 
-            },
-            commentId: {
-                type: Number,
-                required: true 
-            }
-        }, 
-        setup(props) {
-            const like = async () => {
-                const data = await useLike({
-                    type: 'COMMENT',
-                    postId: props.postId,
-                    commentId: props.commentId 
-                })
-                console.log("Data from useLike", data)
-            }
-            
-            return { like } 
+import { defineComponent, ref, computed, onMounted, onBeforeUnmount } from 'vue';
+import { useLike } from '@/composables/useLike';
+import { CommentLikes } from '@/types/posts/CommentLikes';
+import { useSocket } from '@/composables/useWebSocket';
+
+export default defineComponent({
+    name: 'CommentLikes',
+    props: {
+        likes: {
+            type: Array as () => CommentLikes[],
+            required: true
+        },
+        postId: {
+            type: Number,
+            required: true
+        },
+        commentId: {
+            type: Number,
+            required: true
         }
-    })
+    },
+    setup(props) {
+        const userId = Number(localStorage.getItem('userId'));
+
+        const likes = ref<CommentLikes[]>([...(props.likes || [])]);
+        
+        const isLiked = ref<boolean>(likes.value.some(like => like.user_id === userId));
+        
+        const likesCount = computed(() => likes.value.length);
+
+        const { socket } = useSocket();
+
+        const like = async () => {
+            const data = await useLike({
+                type: 'COMMENT',
+                postId: props.postId,
+                commentId: props.commentId
+            });
+            console.log("Data from useLike", data);
+        };
+
+        onMounted(() => {
+            socket.instance.on("likeComment", (liked: CommentLikes) => {
+                if (liked.comment_id === props.commentId)  {
+                    
+                    likes.value.push(liked);
+                    
+                    if (liked.user_id === userId) isLiked.value = true;
+                    
+                    console.log("Comment liked via WebSocket:", liked);
+                }
+            });
+
+            socket.instance.on("unlikeComment", (liked: CommentLikes) => {
+                if (liked.comment_id === props.commentId) {
+                    
+                    likes.value = likes.value.filter(l => l.user_id !== liked.user_id);
+                    
+                    if (liked.user_id === userId) isLiked.value = false;
+                    
+                    console.log("Comment unliked via WebSocket:", liked);
+                }
+            });
+        });
+
+        onBeforeUnmount(() => {
+            socket.instance.off("likeComment");
+            socket.instance.off("unlikeComment");
+        });
+
+        return { like, likesCount, isLiked };
+    }
+});
 </script>
